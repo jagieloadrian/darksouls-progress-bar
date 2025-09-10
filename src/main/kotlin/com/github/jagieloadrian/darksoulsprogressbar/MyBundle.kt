3508b700e29aea3b1cc5d2e72ae6a6f799ac7483
GitHub Action <action@github.com>
2025-09-10 08:30:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/MyBundle.kt
+++ src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jagieloadrian.darksoulsprogressbar
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jagieloadrian.darksoulsprogressbar.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jagieloadrian.darksoulsprogressbar.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
