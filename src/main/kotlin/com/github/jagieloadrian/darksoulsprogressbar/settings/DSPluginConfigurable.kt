51b557eb7876bd97f9b5661e792d643cd3988c54
Adrian J <anjo@gft.com>
2025-11-25 21:31:17 +0100

    extract UI

--- src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/settings/DSPluginConfigurable.kt
+++ src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/settings/DSPluginConfigurable.kt
@@ -1,22 +1,14 @@
 package com.github.jagieloadrian.darksoulsprogressbar.settings
 
-import com.github.jagieloadrian.darksoulsprogressbar.model.Icons
+import com.github.jagieloadrian.darksoulsprogressbar.ui.DSPluginConfiguratorApi
+import com.github.jagieloadrian.darksoulsprogressbar.ui.DSPluginConfiguratorUI
 import com.github.jagieloadrian.darksoulsprogressbar.utils.Names.SETTINGS_NAME
-import com.github.jagieloadrian.darksoulsprogressbar.utils.Names.TEST_FAILURE_WINDOW_DESC
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.util.NlsContexts
-import com.intellij.ui.components.JBScrollPane
-import java.awt.BorderLayout
-import java.awt.Dimension
-import java.awt.FlowLayout
-import javax.swing.BoxLayout
-import javax.swing.ImageIcon
 import javax.swing.JCheckBox
 import javax.swing.JComponent
-import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.SwingConstants
 
 class DSPluginConfigurable(
     private val configPanel: DSPluginConfiguratorApi = DSPluginConfiguratorUI,
@@ -80,62 +72,6 @@ class DSPluginConfigurable(
             animationCheckBox.removeActionListener { }
         }
     }
-}
-
-interface DSPluginConfiguratorApi {
-    fun getPanel(
-        animationEnabled: Boolean,
-        settings: DSPersistentState,
-        assignCheckBoxToPath: (String, JCheckBox) -> Unit,
-    ): ConfigPanelResult
-}
-
-object DSPluginConfiguratorUI : DSPluginConfiguratorApi {
-    override fun getPanel(
-        animationEnabled: Boolean,
-        settings: DSPersistentState,
-        assignCheckBoxToPath: (String, JCheckBox) -> Unit,
-    ): ConfigPanelResult {
-
-        val panel = JPanel(BorderLayout(10, 10))
-        val content = JPanel().apply {
-            layout = BoxLayout(this, BoxLayout.Y_AXIS)
-        }
-
-        Icons.entries.forEach { icon ->
-            val checkBox = JCheckBox(icon.iconName).apply {
-                horizontalAlignment = SwingConstants.LEFT
-                isSelected = icon.path in settings.iconPaths
-            }
-
-            val iconLabel = JLabel(ImageIcon(javaClass.getResource(icon.path))).apply {
-                horizontalAlignment = SwingConstants.CENTER
-            }
-
-            JPanel(FlowLayout(FlowLayout.LEFT)).apply {
-                add(iconLabel)
-                add(checkBox)
-                content.add(this)
-            }
-
-            assignCheckBoxToPath(icon.path, checkBox)
-        }
-
-        val animationCheckBox = JCheckBox(TEST_FAILURE_WINDOW_DESC).apply {
-            isSelected = animationEnabled
-        }
-
-        val scroll = JBScrollPane(content).apply {
-            preferredSize = Dimension(400, 250)
-        }
-
-        panel.add(scroll, BorderLayout.CENTER)
-        panel.add(animationCheckBox, BorderLayout.SOUTH)
-        panel.accessibleContext.accessibleName = "DSPluginConfigPanel"
-
-        return ConfigPanelResult(panel, animationCheckBox)
-    }
-
 }
 
 data class ConfigPanelResult(
