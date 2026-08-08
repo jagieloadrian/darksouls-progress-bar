2b5a642d53258ea7d78c076f9155dba5a2365ce4
diether18 <jagielo.adrian@gmail.com>
2026-08-08 11:13:45 +0200

    refactor(config): restructure items config into nested groups

--- src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/utils/Items.kt
+++ src/main/kotlin/com/github/jagieloadrian/darksoulsprogressbar/utils/Items.kt
@@ -1,14 +1,20 @@
 package com.github.jagieloadrian.darksoulsprogressbar.utils
 
+@Suppress("UNCHECKED_CAST")
 object Items {
     private val config = loadYamlResource("/config/items.yaml")
+    private val gify = config["gify"] as Map<String, String>
+    private val sound = config["sound"] as Map<String, String>
+    private val soundtrack = config["soundtrack"] as Map<String, String>
 
-    val YOU_DIED_GIF: String = config["youDiedGif"] as String
-    val YOU_DIED_SOUND: String = config["youDiedSound"] as String
-    val SMALL_BONFIRE_GIF: String = config["smallBonfireGif"] as String
-    val BACKGROUND_PROGRESS_BAR_GIF: String = config["backgroundProgressBarGif"] as String
-    val DS_ONE_SOUNDTRACK: String = config["dsOneSoundtrack"] as String
-    val DS_TWO_SOUNDTRACK: String = config["dsTwoSoundtrack"] as String
-    val DS_THREE_SOUNDTRACK: String = config["dsThreeSoundtrack"] as String
-    val ELDEN_RING_SOUNDTRACK: String = config["eldenRingSoundtrack"] as String
+    val YOU_DIED_GIF: String = gify.getValue("youDiedGif")
+    val SMALL_BONFIRE_GIF: String = gify.getValue("smallBonfireGif")
+    val BACKGROUND_PROGRESS_BAR_GIF: String = gify.getValue("backgroundProgressBarGif")
+
+    val YOU_DIED_SOUND: String = sound.getValue("youDiedSound")
+
+    val DS_ONE_SOUNDTRACK: String = soundtrack.getValue("dsOneSoundtrack")
+    val DS_TWO_SOUNDTRACK: String = soundtrack.getValue("dsTwoSoundtrack")
+    val DS_THREE_SOUNDTRACK: String = soundtrack.getValue("dsThreeSoundtrack")
+    val ELDEN_RING_SOUNDTRACK: String = soundtrack.getValue("eldenRingSoundtrack")
 }
